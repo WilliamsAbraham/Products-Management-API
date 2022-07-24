1e7c846102690eef63d59bf00028587597236fca
Abraham <68301663+WilliamsAbraham@users.noreply.github.com>
2022-07-24 20:32:18 +0100

    ProjectSetUp

---
+++ Product-Management-API/Entities/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Product
+    {
+        [Column("ProductId")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Product name is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
+        public string? ProductName { get; set; }
+        public string? ProductDescription { get; set; }
+
+        [Required(ErrorMessage = "Price is a required field.")]
+        public decimal Price { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+
+    }
+}
---
+++ Product-Management-API/Product-Management-API/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repository;
+
+namespace Product_Management_API.ContextFactory
+{
+    public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+
+    {
+        public RepositoryContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()  
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+            var builder = new DbContextOptionsBuilder<RepositoryContext>()
+            .UseMySql(configuration.GetConnectionString("sqlConnection"),
+            b => b.MigrationsAssembly("Product-Management-API"));
+            return new RepositoryContext(builder.Options);
+
+        }
+    }
+}
