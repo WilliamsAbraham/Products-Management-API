78066c21c96ff6618f450bda7dc83df923b66ebf
Abraham <68301663+WilliamsAbraham@users.noreply.github.com>
2022-07-25 02:38:09 +0100

    Update

---
+++ Product-Management-API/Contracts/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<CompanyDto>> GetAllProductsAsync(bool trackChanges, CancellationToken cancellationToken);
+        Task<CompanyDto> CreateCompanyAsync(CompanyForCreationDto company,CancellationToken cancellationToken);
+        Task DeleteCompanyAsync(Guid companyId, bool trackChanges, CancellationToken cancellationToken);
+        Task UpdateCompanyAsync(Guid companyid, CompanyForUpdateDto companyForUpdate,bool trackChanges, CancellationToken cancellationToken);
+    }
+}
---
+++ Product-Management-API/Contracts/IRepositoryBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public interface IRepositoryBase<T>
+    {
+        Task<IQueryable<T>> FindAll(bool trackChanges,CancellationToken cancellationToken );
+        Task Create(T entity,CancellationToken cancellationToken);
+        Task Update(T entity, CancellationToken cancellationToken);
+        Task Delete(T entity, CancellationToken cancellationToken);
+
+    }
+}
