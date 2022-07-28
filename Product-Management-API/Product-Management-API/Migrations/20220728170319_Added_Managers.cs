e5bf41b9ec0dbf7182d3bb37f84d824e4b028003
Abraham <68301663+WilliamsAbraham@users.noreply.github.com>
2022-07-28 18:07:50 +0100

    Update

---
+++ Product-Management-API/Product-Management-API/Migrations/20220728170319_Added_Managers.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Product_Management_API.Migrations
+{
+    public partial class Added_Managers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5c78bd2a-d5be-4b27-8513-aa7ec3641137");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c7d0208d-a2e6-469c-843d-f36c09fa4c2b");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "1bd6f452-4165-4347-a3ab-e65936ad087b", "a209aac0-33ef-49bc-8d3c-3b01198fece1", "Manager", "MANAGER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "42569b1e-b43a-46cb-9361-79dabfafe33a", "49410f72-3e0d-4051-b038-e806143c9754", "Staff", "ADMINISTRATOR" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1bd6f452-4165-4347-a3ab-e65936ad087b");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "42569b1e-b43a-46cb-9361-79dabfafe33a");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "5c78bd2a-d5be-4b27-8513-aa7ec3641137", "ab5a2630-5f9b-4d06-a15d-93af4b6b378b", "Manager", "MANAGER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "c7d0208d-a2e6-469c-843d-f36c09fa4c2b", "f9be8a4d-e878-44ca-a860-7194ced5cf4b", "Staff", "ADMINISTRATOR" });
+        }
+    }
+}
--- Product-Management-API/Repository/Configurations/ManagerConfiguration.cs
+++ Product-Management-API/Repository/Configurations/ManagerConfiguration.cs
@@ -1,16 +1,20 @@
 ﻿using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Repository.Configurations
 {
-    public class ManagerConfiguration
+    public class ManagerConfiguration : IEntityTypeConfiguration<IdentityUser>
     {
-        public void Configure(EntityTypeBuilder<AppAdmin> builder)
+        public void Configure(EntityTypeBuilder<IdentityUser> builder)
         {
             //Seeding the Admin table with the manger User.
             builder.HasData(
                 new AppAdmin { FirstName = "Abraham", LastName = "Williams", Password = "AB123", Email = "willingtoexcel@gmail.com", Role = "Manager" });
                 
         }
+
+        
     }
 }
